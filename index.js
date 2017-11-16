53ffc3e4824554b357afa9aeb047ee0eed7c40c6
Darryl <darryl.github@gmail.com>
2017-11-15 19:13:09 -0800

    initial-base-build

---
+++ index.js
@@ -0,0 +1,260 @@
+
+/* Variables */
+const discord = require('discord.js');
+
+const discord_api_token = process.env.DISCORD_TOKEN;
+const client = new discord.Client();
+const ontime = {};
+const available = [];
+
+const market = {};
+
+const response = require('./response');
+
+function load_available(database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {available.push(table[row]['raid'])}
+  });
+}
+
+
+/* Blocks */
+function initiate_raid(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+
+    for (let row in table) {if (table[row]['raid'] === message.channel.name){return}}
+    collection.insertMany([{raid:message.channel.name,'active':false,members:{}}]);
+    available.push(message.channel.name);
+  });
+
+  message.channel.send(response.initiate(discord, message)).then(msg => {
+    msg.pin()
+  });;
+}
+
+function join_raid(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name) {
+
+        let raid_members = table[row]['members'];
+        for (let user in raid_members) {if (user === message.author.username) {return}}
+
+        raid_members[message.author.username] = 0;
+        collection.updateOne({'raid': message.channel.name}, { $set:{'members':raid_members }});
+      }
+    }
+  });
+
+  if (ontime[message.channel.name] === false) {return}
+  for (let user in ontime[message.channel.name]) {if (user === message.author.username) {return}}
+  ontime[message.channel.name].push(message.author.username);
+
+  message.channel.send(response.join(discord, message));
+}
+
+function start_raid(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] === false && table[row]['raid'] === message.channel.name) {
+        collection.updateOne({'raid': message.channel.name}, { $set:{'active':true }});
+        ontime[message.channel.name]=[message.author.username];
+
+        setTimeout(() => {
+          message.channel.send(response.raidReady(discord, message));
+          award(message, database, 1);
+        }, (15*60000));
+      }
+    }
+  });
+
+  message.channel.send(response.raidStart(discord, message));
+}
+
+function end_raid(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name){
+        collection.updateOne({'raid': message.channel.name}, { $set:{'active':false }});
+        ontime[message.channel.name] = false;
+      }
+    }
+  });
+
+  message.channel.send(response.raidEnd(discord, message));
+}
+
+function award(message, database, option) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name && option > 0) {
+        let raid_members = table[row]['members'];
+        for (let user in raid_members) {if (ontime[message.channel.name].indexOf(user) > -1) {raid_members[user] += parseInt(option)}}
+        collection.updateOne({'raid': message.channel.name}, { $set:{'members':raid_members }});
+
+        message.channel.send(response.award(discord, message, option));
+      }
+    }
+  });
+}
+
+function balance(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name) {
+        for (let user in table[row]['members']) {if (message.author.username === user) {
+          message.author.send(response.raidBalance(discord, message, table[row]['members'][user]));
+        }}
+      }
+    }
+  });
+}
+
+function display(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+
+    var active = {};
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name) {
+        for (let user in table[row]['members']) {active[user] = table[row]['members'][user]}
+      }
+    }
+
+    var view = ''; for (let user in active) {if (ontime[message.channel.name].indexOf(user) > -1) {view = view + `\n**${active[user]}** *${user}*`}}
+    message.channel.send(response.raidDisplay(discord, message, view));
+  });
+}
+
+function check(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name) {
+        let embed = new discord.RichEmbed()
+          .setColor('#fc0294')
+          .setDescription(`**${ontime[message.channel.name].length}** players have joined!`);
+        message.channel.send({embed});
+      }
+    }
+  });
+}
+
+
+function deduct(message, database, player, option) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name) {
+        let raid_members = table[row]['members'];
+        for (let user in raid_members) {if (ontime[message.channel.name].indexOf(user) > -1 && player === user) {raid_members[user] -= parseInt(option)}}
+        collection.updateOne({'raid': message.channel.name}, { $set:{'members':raid_members }});
+      }
+    }
+  });
+}
+
+function auction(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] === true && table[row]['raid'] === message.channel.name) {
+
+        market[message.channel.name] = {'active':true, 'player': message.author.username, 'bid':0}
+        message.channel.send(response.auction(discord));
+
+      }
+    }
+  });
+}
+
+function gavel(message, database) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] === true && table[row]['raid'] === message.channel.name && market[message.channel.name].active) {
+
+        market[message.channel.name].active = false;
+        deduct(message, database, market[message.channel.name].player, market[message.channel.name].bid);
+
+        message.channel.send(response.gavel(discord, message, market[message.channel.name].player));
+      }
+    }
+  });
+}
+
+function bid(message, database, option) {
+  var collection = database.collection('raid-groups');
+  collection.find({}).toArray((err, table) => {
+    for (let row in table) {
+      if (table[row]['active'] && table[row]['raid'] === message.channel.name && market[message.channel.name].active) {
+        let raid_members = table[row]['members'];
+        for (let user in raid_members) {if (ontime[message.channel.name].indexOf(user) > -1 && message.author.username === user) {
+          if (parseInt(option) < parseInt(raid_members[user]) && parseInt(option) > market[message.channel.name].bid) {
+            market[message.channel.name].player = message.author.username;
+            market[message.channel.name].bid = parseInt(option);
+
+            message.channel.send(response.newBid(discord, message, option));
+          }
+        }}
+      }
+    }
+  });
+}
+
+
+/* Handler */
+var mDB = require('mongodb').MongoClient;
+mDB.connect(process.env.MONGODB_URI, (err, database) => {
+
+  client.on('ready', () => {
+    load_available(database);
+    console.log('nodkp-initiated');
+  });
+
+  client.on("presenceUpdate", (userold, usernew) => {
+    if (userold.presence.status === 'offline' && usernew.presence.status === 'online') {
+      console.log('presence-update');
+    }
+  });
+
+  client.on('message', (message) => {
+    if (message.author.bot) {return;}
+    if (message.isMentioned(client.user) && message.channel.type === 'text') {
+      var option = message.content.split(' ');
+
+      if (message.member.roles.some(r=>['Raid Leader'].includes(r.name)) === true) {
+        if (option[1] === 'init') {initiate_raid(message, database)}
+      }
+      if (available.indexOf(message.channel.name) > -1) {
+        if (option[1] === 'join') {join_raid(message, database)}
+        if (option[1] === 'balance') {balance(message, database)}
+        
+        if (option[1] === 'bid') {bid(message, database, option[2])}
+
+        if (message.member.roles.some(r=>['Raid Leader'].includes(r.name)) === true) {
+          if (option[1] === 'start') {start_raid(message, database)}
+          if (option[1] === 'end') {end_raid(message, database)}
+          if (option[1] === 'award') {award(message, database, option[2])}
+          if (option[1] === 'display') {display(message, database)}
+          if (option[1] === 'check') {check(message, database)}
+
+          if (option[1] === 'auction') {auction(message, database)}
+          if (option[1] === 'gavel') {gavel(message, database)}
+
+        }
+      }
+
+      message.delete();
+    }
+  });
+
+  client.login(discord_api_token);
+});
---
+++ response.js
@@ -0,0 +1,102 @@
+module.exports = {
+
+  initiate: (discord, message) => {
+    let embed = new discord.RichEmbed()
+      .setTitle('nodkp')
+      .setColor('#d1f442')
+      .setThumbnail("https://cdn2.iconfinder.com/data/icons/helmet/512/warrior-soldier-helmet-war-512.png")
+      .setDescription(
+          `**${message.author.username}** created **${message.channel.name}** raid group!\n\n`+
+          `Hello I am nodkp! I am here to assist with dkp management. Below are the commands available. All raid leaders must have a rank named 'Raid Leader' to use leader commands.\n`+
+          `\n - *developed by* [*Softban*](https://github.com/softban/nodkp)`
+      )
+      .addBlankField(true)
+      .addField('start', '(**leader**) *Starts the raid, allowing players to join. After 15 minutes, the raid will begin and 1dkp will be awarded for all players ontime.*')
+      .addField('end', '(**leader**) *Ends the raid, disables commands, and resets ontime for next raid.*')
+      .addField('award [int]', '(**leader**) *Requires an amount, (@bot award 10). Awards all players who have join the raid with the specified amount.*')
+      .addField('display', '(**leader**) *Show entire raids balance.*')
+      .addField('check', '(**leader**) *Show number of players joined.*')
+      .addField('auction', '(**leader**) *Start a new auction.*')
+      .addField('gavel', '(**leader**) *End current auction, winning player will have dkp deducted for item.*')
+      .addBlankField(true)
+      .addField('join', '(**player**) *Joins the raid.*')
+      .addField('balance', '(**player**) *Show personal dkp balance.*')
+      .addField('bid [int]', '(**player**) *Requires an amount, (@bot bid 10). Allows players to bid their dkp for current auction item.*')
+      .setTimestamp();
+    return embed;
+  },
+
+  join: (discord, message) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#41f4bb')
+      .setDescription(`**${message.author.username}** has joined!\n`);
+    return embed;
+  },
+
+  raidStart: (discord, message) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#abfca6')
+      .setDescription(`**${message.channel.name}** raid will being in **15 minutes**!`);
+    return embed;
+  },
+
+  raidReady: (discord, message) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#4cf441')
+      .setDescription(`**${message.channel.name}** raid has begun!`);
+    return embed;
+  },
+
+  raidEnd: (discord, message) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#e56791')
+      .setDescription(`**${message.channel.name}** raid has ended!`);
+    return embed;
+  },
+
+  award: (discord, message, option) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#ffb06b')
+      .setDescription(`**${message.channel.name}** raiders have been awarded **${option}**!`);
+    return embed;
+  },
+
+  raidBalance: (discord, message, balance) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#b2fff5')
+      .setDescription(`balance: **${balance}**`);
+    return embed;
+  },
+
+  raidDisplay: (discord, message, view) => {
+    let embed = new discord.RichEmbed()
+      .setTitle('DKP Balance')
+      .setColor('#67f7e6')
+      .setDescription(`${view}`)
+      .setTimestamp();
+    return embed;
+  },
+
+  auction: (discord) => {
+    let embed = new discord.RichEmbed()
+      .setColor('#c384ff')
+      .setTitle(`Auction has begun, now accepting bids!`);
+    return embed;
+  },
+
+  gavel: (discord, message, player) => {
+    let embed = new discord.RichEmbed()
+      .setTitle(`Auction has ended!`)
+      .setColor('#c384ff')
+      .setDescription(`**${player}** has won the auction!`);
+    return embed;
+  },
+
+  newBid: (discord, message, option) => {
+    let embed = new discord.RichEmbed()
+      .setTitle('Highest Bid')
+      .setColor('#c384ff')
+      .setDescription(`**${message.author.username}** ${option}`);
+    return embed;
+  }
+}
