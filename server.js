7b3ddf6d0bc395359d6b4f5cc0a8fb0e50690344
Darryl <darryl.github@gmail.com>
2017-12-07 20:16:02 -0800

    ...

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, 'view/index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
