ebe2eb2ed742e774d6acd95ebe9151552814cc7c
Darryl <darryl.github@gmail.com>
2017-12-07 20:14:07 -0800

    ...

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-
-const path = require('path');
 const express = require('express');
-const socketio = require('socket.io')
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .listen(process.env.WEBPORT, () => console.log(`[+] listening on ${process.env.WEBPORT}`));
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketio(server);
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-server.get("/", function(req, res) {
- res.send("Heroku Demo!");
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
